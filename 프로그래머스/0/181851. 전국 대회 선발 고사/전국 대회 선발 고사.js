93428198af590095750147725be3abbd8652630c
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2024-04-18 09:25:13 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,6 +1,7 @@
 function solution(rank, attendance) {
-    rank = rank.map((r, i) => ({i: i, r: r}));
-    rank = rank.filter((r, i) => attendance[i]).sort((a, b) => a['r'] - b['r']);
-    const a = rank[0]['i']; const b = rank[1]['i']; const c = rank[2]['i'];
-    return 10000 * a + 100 * b + c;
+    const [a, b, c] = rank
+        .map((r, i) => [r, i])
+        .filter((_, i) => attendance[i])
+        .sort(([a], [b]) => a - b);
+    return 10000 * a[1] + 100 * b[1] + c[1];
 }
