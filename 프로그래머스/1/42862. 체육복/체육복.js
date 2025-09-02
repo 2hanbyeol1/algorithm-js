21fca34956cd209cd451be9a1a5b7abcbb5ca95a
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2025-09-02 18:58:42 +0900

    [level 1] Title: 체육복, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,22 +1,17 @@
-function solution(n, lost, reserve) {
-    let lost_filter, reserve_filter;
+function solution(n, lost, reserve) {    
+    // 여벌 체육복을 가져온 학생이 체육복을 도난당한 경우,
+    // 다른 학생에게는 체육복을 빌려줄 수 없음
+    const losts = lost
+        .filter((l) => !reserve.includes(l))
+        .sort((a, b) => a - b);
+    const reserves = reserve
+        .filter((r) => !lost.includes(r))
+        .sort((a, b) => a - b);
     
-    // 여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다.
-    // 다른 학생에게는 체육복을 빌려줄 수 없습니다.
-    lost_filter = lost.filter(l => !reserve.includes(l)).sort((a, b) => a-b)
-    reserve_filter = reserve.filter(r => !lost.includes(r)).sort((a, b) => b-a) // 숫자가 큰 학생에게 빌리는게 우선
-    
-    // 여벌 체육복이 있는 학생만 다른 학생에게 체육복을 빌려줄 수 있습니다.
-    for(var i = lost_filter.length-1; i >= 0; i--) {
-        const l = lost_filter[i]
-        idx = reserve_filter.findIndex(r => r === l-1 || r === l+1)
-        console.log(lost_filter, reserve_filter)
-        if(idx >= 0) {
-            console.log('can borrow')
-            lost_filter.splice(i, 1)
-            reserve_filter.splice(idx, 1)
-        }
-        console.log(l, idx, lost_filter, reserve_filter)
-    }
-    return n - lost_filter.length;
+    return n - losts.filter((l) => {
+        const idx = reserves.findIndex((r) => Math.abs(r - l) === 1);
+        const hasReserve = idx >= 0;
+        if(hasReserve) reserves.splice(idx, 1);
+        return !hasReserve;
+    }).length;
 }
