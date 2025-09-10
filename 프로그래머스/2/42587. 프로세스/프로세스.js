19bb785baf404920726852c8ee0e7448bb37d1dc
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2025-09-10 11:34:52 +0900

    [level 2] Title: 프로세스, Time: 0.55 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,18 +1,21 @@
 function solution(priorities, location) {
-    var answer = 0;
-    // 0) 실행 대기 큐 q
-    // {p, idx} 형태로 저장
-    const q = priorities.map((p, idx) => ({p, idx}));
+    // 실행 대기 큐 q
+    const q = priorities.map((p, idx) => ({p, idx})); // {p, idx} 형태로 저장
+    // 프로세스 실행 횟수 cnt
+    let cnt = 0;
+    
     while (true) {
         // 1) q 에서 프로세스 하나 p 를 꺼냄
         const p = q.shift();
-        // 2-1) q 에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면,
-        // p 를 다시 큐에 넣습니다.
-        if(!!q.find((pending) => pending.p > p.p)) q.push(p)
-        // 2-2) 만약 그런 프로세스가 없다면, 방금 꺼낸 프로세스를 실행합니다.
+        
+        // 2-1) q 에 대기 중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면, p 를 큐에 넣음
+        if(q.some((pending) => pending.p > p.p)) q.push(p)
+        // 2-2) 만약 그런 프로세스가 없다면,
         else {
-            answer++;
-            if(p.idx === location) return answer;
+            // 3) 프로세스 실행 -> 프로세스 실행 횟수 증가
+            cnt++;
+            // 3-1) 실행한 프로세스가 location에 있다면 프로세스 실행 횟수 return
+            if(p.idx === location) return cnt;
         }
     }
     
