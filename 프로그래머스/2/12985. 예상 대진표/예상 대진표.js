2dda67cc2b7df1d05ffdf6105535d3b930e0512e
한별 <78421872+2hanbyeol1@users.noreply.github.com>
2025-10-01 15:38:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -3,10 +3,10 @@
 // b B번 참가자
 function solution(n, a, b) {
     let answer = 0;
-    while(a !== b) {
-        a = Math.ceil(a / 2); // 4 -> 2 -> 1 -> 1
-        b = Math.ceil(b / 2); // 7 -> 4 -> 2 -> 1
-        answer++;             // 0 -> 1 -> 2 -> 3
+    while(true) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        answer++;
+        if(a === b) return answer;
     }
-    return answer;
 }
